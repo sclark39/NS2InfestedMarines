f7335fac1d42fb003db2bd36cd373f87be5714a4
Trevor Harris <trevorharrisvfx@gmail.com>
2016-10-27 16:32:54 -0500

    Disable all but certain specified achievements from being unlocked during the gamemode.  (Currently, none are specified, so none are allowed).

--- InfestedMarines/source/lua/IMClient.lua
+++ InfestedMarines/source/lua/IMClient.lua
@@ -15,3 +15,15 @@ Script.Load("lua/IMGUIObjectivesMarine.lua")
 Script.Load("lua/IMGUIObjectivesAlien.lua")
 Script.Load("lua/IMGUIInfestedFeedTimer.lua")
 Script.Load("lua/IMGUIInfestedOverlay.lua")
+
+--map of achievements that are available in this gamemode
+local allowedAchievements =
+{
+}
+
+local oldSetAchievement = Client.SetAchievement
+function Client.SetAchievement( name )
+    if allowedAchievements[name] then
+        oldSetAchievement( name )
+    end
+end

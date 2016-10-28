2214eff5154e446284266addf7a8a86e94948e4a
sclark39 <sclark@freolic.com>
2016-10-27 22:36:48 -0400

    Fix hiding of ui elements

--- InfestedMarines/source/lua/IMGUIMarineHUD.lua
+++ InfestedMarines/source/lua/IMGUIMarineHUD.lua
@@ -1,14 +1,20 @@
+
+
+    
+
+
 local originalHUDUpdate
 originalHUDUpdate = Class_ReplaceMethod( "GUIMarineHUD", "Update", function(self)
 	originalHUDUpdate(self)
+    
 	self.commanderName:SetIsVisible(false)
 	self.resourceDisplay.teamText:SetIsVisible(false)
-
+	self.resourceDisplay.background:SetIsVisible(false)
 	if self.minimapBackground then
 		self.minimapBackground:SetIsVisible(false)
-	end
-
-	if self.gameTime then
-		self.gameTime:SetIsVisible(false)
-	end
+    end
+    
+	--if self.gameTime then
+		--self.gameTime:SetIsVisible(false)
+	--end
 end)

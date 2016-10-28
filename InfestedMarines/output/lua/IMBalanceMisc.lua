e3cf3be5bc0f651ca10177d6082ae4ce94cb6fba
sclark39 <sclark@freolic.com>
2016-10-27 20:47:16 -0400

    Remove all the output files

--- InfestedMarines/output/lua/IMBalanceMisc.lua
+++
@@ -1,9 +0,0 @@
--- ======= Copyright (c) 2003-2010, Unknown Worlds Entertainment, Inc. All rights reserved. =======
---
--- lua\IMBalanceMisc.lua
---
---    Created by:   Trevor Harris (trevor@naturalselection2.com)
---
--- ========= For more information, visit us at http://www.unknownworlds.com =====================
-
-kStructureWeldRate = 900

d471f1c37d3f929a496fe1d3130bb2437156295b
Trevor Harris <trevorharrisvfx@gmail.com>
2016-11-15 19:17:40 -0600

    Revert "Added all Infested Marines strings to Locale, and use that now."
    This reverts commit 0d4750eee1a967126c777ede91845877943d9926.

--- InfestedMarines/source/lua/IMGUIAirStatus.lua
+++ InfestedMarines/source/lua/IMGUIAirStatus.lua
@@ -180,10 +180,6 @@ local function SharedUpdate(self, deltaTime)
         end
     end
     
-    -- update text, just in case they changed locales.
-    self.text:SetText(IMStringGetAirQualityMessage())
-    self.textShadow:SetText(self.text:GetText())
-    
 end
 
 function IMGUIAirStatus:Initialize()
@@ -199,7 +195,7 @@ function IMGUIAirStatus:Initialize()
     self.text:SetFontName(IMGUIAirStatus.kFont)
     self.text:SetTextAlignmentX(GUIItem.Align_Center)
     self.text:SetTextAlignmentY(GUIItem.Align_Center)
-    self.text:SetText(IMStringGetAirQualityMessage())
+    self.text:SetText("AIR QUALITY")
     
     self.textShadow = GUIManager:CreateGraphicItem()
     self.textShadow:SetAnchor(GUIItem.Middle, GUIItem.Top)
--- InfestedMarines/source/lua/IMStrings.lua
+++ InfestedMarines/source/lua/IMStrings.lua
@@ -8,73 +8,112 @@
 --
 -- ========= For more information, visit us at http://www.unknownworlds.com =====================
 
+---------------------
+-- STRING LITERALS --
+---------------------
+-- Here for now.  Will later be moved to enUS, and be loaded via Locale.
+
+local kInfestedMessage = "You are infested!\nRight-click (default for alt-fire) marines when close enough (outline turns red) to infest them.  Pretend you're uninfested to avoid suspicion, but you will starve to death soon if you do not infest more humans!"
+
+local kNotInfestedMessage = "The infested has been chosen!\nYou are not infested!  One or more of your \"friends\" however, are.  Watch each other closely.  Infestation is spreading throughout the facility and poisoning the air!  Clear the infestation and repair the Air Purifiers before the air becomes lethal!"
+
+local kNobodyInfestedMessage = "Infestation is taking over the facility; the air is becoming toxic!  Repair the Air Purifiers before the air becomes lethal.  We cannot lose this facility!\n\n(Infested has not yet been chosen.)"
+
+local kRightClickTipMessage = "Infest (get close)"
+
+local kDoNotWeldPurifiersMessage = "You are helping the enemy!  Do not repair Air Purifiers except to avoid suspicion."
+
+local kDoNotKillCystsMessage = "You are helping the enemy!  Do not kill cysts except to avoid suspicion."
+
+local kKillCystsMessage = "Those cysts are emitting toxic gas.  Burn them!"
+
+local kWeldPurifiersMessage = "There is a damaged air purifier nearby.  Damaged purifiers filter the toxins out of the air less effectively.  Weld them!"
+
+local kFriendlyFireVictimMessage = "You were burned by a teammate!  They must have thought you were infested.  Keep your distance from other players, and try not to act suspicious."
+
+local kFriendlyFireAttackerMessage = "You burned an innocent, uninfested teammate!  If they were acting crazy, then bad luck, they probably deserved it.  But if they were being reasonable, maybe give them the benefit of the doubt next time."
+
+local kInfestedSuicideByFlamethrowerMessage = "You attempted to burn a perfectly good host!  You violated the primary directive of the infested: to spread."
+
+local kInfestedFriendlyFireMessage = "You cannot kill other infested."
+
+local kFeedSoonMessage = "You are close to starvation!  Infest another player soon!"
+
+local kFeedDeathMessage = "You starved to death!  Infest marines to keep yourself from starving."
+
+local kSuffocatedDeathMessage = "You suffocated on the toxic air!  Weld Air Purifiers and kill Cysts to keep the air clean!"
+
+------------------------
+-- ACCESSOR FUNCTIONS --
+------------------------
+-- These will not be replaced later, but will be modified to return the Locale string, rather
+-- than the above English literals.
+
 function IMStringGetBlankMessage()
     return ""
 end
 
 function IMStringGetInfestedMessage()
-    return Locale.ResolveString("INFESTED_MARINES_INFESTED_MESSAGE")
+    return kInfestedMessage
 end
 
 function IMStringGetNotInfestedMessage()
-    return Locale.ResolveString("INFESTED_MARINES_NOT_INFESTED_MESSAGE")
+    return kNotInfestedMessage
 end
 
 function IMStringGetNobodyInfestedMessage()
-    return Locale.ResolveString("INFESTED_MARINES_NOBODY_INFESTED_MESSAGE")
+    return kNobodyInfestedMessage
 end
 
 function IMStringGetRightClickTipMessage()
-    return Locale.ResolveString("INFESTED_MARINES_RIGHT_CLICK_TIP_MESSAGE")
+    return kRightClickTipMessage
 end
 
 function IMStringGetDoNotWeldPurifiersMessage()
-    return Locale.ResolveString("INFESTED_MARINES_DO_NOT_WELD_PURIFIERS_MESSAGE")
+    return kDoNotWeldPurifiersMessage
 end
 
 function IMStringGetDoNotKillCystsMessage()
-    return Locale.ResolveString("INFESTED_MARINES_DO_NOT_KILL_CYSTS_MESSAGE")
+    return kDoNotKillCystsMessage
 end
 
 function IMStringGetKillCystsMessage()
-    return Locale.ResolveString("INFESTED_MARINES_KILL_CYSTS_MESSAGE")
+    return kKillCystsMessage
 end
 
 function IMStringGetWeldPurifiersMessage()
-    return Locale.ResolveString("INFESTED_MARINES_WELD_PURIFIERS_MESSAGE")
+    return kWeldPurifiersMessage
 end
 
 function IMStringGetFriendlyFireVictimMessage()
-    return Locale.ResolveString("INFESTED_MARINES_FRIENDLY_FIRE_VICTIM_MESSAGE")
+    return kFriendlyFireVictimMessage
 end
 
 function IMStringGetFriendlyFireAttackerMessage()
-    return Locale.ResolveString("INFESTED_MARINES_FRIENDLY_FIRE_ATTACKER_MESSAGE")
+    return kFriendlyFireAttackerMessage
 end
 
 function IMStringGetInfestedSuicideByFlamethrowerMessage()
-    return Locale.ResolveString("INFESTED_MARINES_INFESTED_SUICIDE_BY_FLAMETHROWER_MESSAGE")
+    return kInfestedSuicideByFlamethrowerMessage
 end
 
 function IMStringGetInfestedFriendlyFireMessage()
-    return Locale.ResolveString("INFESTED_MARINES_INFESTED_FRIENDLY_FIRE_MESSAGE")
+    return kInfestedFriendlyFireMessage
 end
 
 function IMStringGetFeedSoonMessage()
-    return Locale.ResolveString("INFESTED_MARINES_FEED_SOON_MESSAGE")
+    return kFeedSoonMessage
 end
 
 function IMStringGetFeedDeathMessage()
-    return Locale.ResolveString("INFESTED_MARINES_FEED_DEATH_MESSAGE")
+    return kFeedDeathMessage
 end
 
 function IMStringGetSuffocatedMessage()
-    return Locale.ResolveString("INFESTED_MARINES_SUFFOCATED_DEATH_MESSAGE")
+    return kSuffocatedDeathMessage
 end
 
-function IMStringGetAirQualityMessage()
-    return Locale.ResolveString("INFESTED_MARINES_AIR_QUALITY")
-end
+
 
 
 
